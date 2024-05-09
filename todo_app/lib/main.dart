60d5fe76457aadeb6321673964edef38340172d6
NekSherazi <neksherazi@gmail.com>
2024-05-09 16:01:57 +0500

    Task 2 Completed

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,8 +1,5 @@
-// ignore_for_file: unused_import
-
 import "package:flutter/material.dart";
-import 'package:todo_app/screens/splash_screen.dart';
-import 'screens/home_screen.dart';
+import './screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +16,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.cyan,
       ),
       title: "Todo App",
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
